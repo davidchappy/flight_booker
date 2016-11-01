54c55b6b209eb6b6883b5c0d841f03ad9c631114
David Chapman <davidchappy@gmail.com>
2016-11-01 11:33:15 -0300

    Add Airport and Flight models and Flight controller/routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161101141115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "airports", force: :cascade do |t|
+    t.string   "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.datetime "departure"
+    t.integer  "duration"
+    t.integer  "origin_id"
+    t.integer  "destination_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,24 @@ if Rails.env.development?
   end
 end
 
+Airport.create!(code: "NYC")
+Airport.create!(code: "SFO")
+Airport.create!(code: "ATL")
+Airport.create!(code: "GSP")
+Airport.create!(code: "LAX")
+Airport.create!(code: "ORD")
+Airport.create!(code: "MDW")
+Airport.create!(code: "MIA")
+Airport.create!(code: "CLT")
+
+8.times do |n|
+  t = Time.now
+  departure = t + (n * 10)
+  duration = 1 * n
+  origin = Airport.all[n-1]
+  destination = Airport.all[n]
+  Flight.create!( departure: departure, 
+                  duration: duration, 
+                  origin: origin, 
+                  destination: destination)
+end
